dde9a06b58d3f5e3d3644e2926fcbfc48ac77ef5
SagasaNotePC <03220322a@gmail.com>
2021-04-06 14:10:51 +0900

    アップデート処理を記述

--- src/main/java/hide/event/gui/GuiCapState.java
+++ src/main/java/hide/event/gui/GuiCapState.java
@@ -1,5 +1,9 @@
 package hide.event.gui;
 
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
 import hide.capture.CaptureManager.CountMap;
 import hide.event.CaptureWar;
 import hide.event.CaptureWar.CapPointData;
@@ -7,6 +11,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.resources.I18n;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -40,7 +45,7 @@ public class GuiCapState {
 
 		drawCapPointState(width / 2, 40);
 
-		drawRect2Color(xCenter/2, 20, xCenter/2, 26, 0xFFFF4444, 0xFF444444);
+		drawRect2Color(xCenter / 2, 20, xCenter / 2, 26, 0xFFFF4444, 0xFF444444);
 	}
 
 	private void drawRect2Color(int centerX, int centerY, int width, int height, int inColor, int outColor) {
@@ -72,7 +77,8 @@ public class GuiCapState {
 		}
 	}
 
-	public static void drawProgress(int left, int top, int right, int bottom, int color_base, int color_over, float progress) {
+	public static void drawProgress(int left, int top, int right, int bottom, int color_base, int color_over,
+			float progress) {
 		Gui.drawRect(left, top, right, bottom, color_base);
 		Gui.drawRect(left, top, (int) (left + (right - left) * progress), bottom, color_over);
 	}
@@ -82,8 +88,59 @@ public class GuiCapState {
 		render.drawString(str, x - width / 2, y - render.FONT_HEIGHT / 2, color);
 	}
 
+	private static final String ViewKey = "hide.gui.capstate.view";
+	private static final String ViewNone = "hide.gui.capstate.viewnone";
 
-	public void updateState(String myTeam,CountMap<String> a) {
+	private float myPoint;
+	private String myString;
 
+	private float enemyPoint;
+	private String enemyString;
+
+	public void updateState(String myTeam, CountMap<String> map) {
+
+		if (map.size() < 2) {
+			// 1件or0件
+			if (map.containsKey(myTeam)) {
+				myString = I18n.format(ViewKey, myTeam, map.get(myTeam), 1);
+				myPoint = 1;
+				enemyString = I18n.format(ViewNone);
+				enemyPoint = 0;
+			} else {
+				myString = I18n.format(ViewKey, myTeam, 0, 2);
+				myPoint = 0;
+
+				Entry<String, Integer> big = map.biggest();
+				if (big != null) {
+					enemyString = I18n.format(I18n.format(ViewKey, big.getKey(), big.getValue(), 1));
+					enemyPoint = 1;
+				} else {
+					enemyString = I18n.format(ViewNone);
+					enemyPoint = 0;
+				}
+			}
+		} else {
+			List<String> list = map.entrySet().stream().sorted((e0, e1) -> e1.getValue() - e0.getValue())
+					.map(entry -> entry.getKey()).collect(Collectors.toList());
+			float total = map.values().stream().collect(Collectors.summingInt(count -> count));
+
+			if (map.containsKey(myTeam)) {
+				myString = I18n.format(I18n.format(ViewKey, myTeam, map.get(myTeam), list.indexOf(myTeam) + 1));
+				myPoint = map.getNum(myTeam) / total;
+			} else {
+				myString = I18n.format(ViewKey, myTeam, 0, map.size() + 1);
+				myPoint = 0;
+			}
+
+			String enemy;
+			// 1位ならenemyに2ndを
+			if (list.indexOf(myTeam) == 0)
+				enemy = list.get(1);
+			else
+				enemy = list.get(0);
+
+			enemyString = I18n.format(I18n.format(ViewKey, enemy, map.get(enemy), list.indexOf(enemy) + 1));
+			enemyPoint = map.getNum(enemy) / total;
+		}
 	}
 }
